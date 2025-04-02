b013c44eab28f12370754d1798938766f303f014
Stuffy06138 <bernardoadribeiro03@gmail.com>
2025-04-02 16:54:58 -0300

    adaptações finais

--- projetoDaves2/src/view/Portifolio.jsx
+++ projetoDaves2/src/view/Portifolio.jsx
@@ -1,49 +1,26 @@
-import Card from "../../components/Card/Card"
-import Titulo from "../../components/Titulo/Titulo"
+import Card from "../components/Card/Card"
+import Titulo from "../components/Titulo/Titulo"
+import RelogioDigital from "../components/RelogioDigital/RelogioDigital"
+import BarraProgresso from "../components/BarraProgresso/BarraProgresso"
+import Votacao from "../components/Votacao/Votacao"
 import "./Portifolio.css"
 
-const Portifolio = ()=>{
-
+const Portifolio = () => {
     return (
         <div className="board">
-            <Titulo valor="Componentes React" />
+            <Titulo valor="Exercícios Daves" />
             
-            <Card titulo="Desafio 01 - Passagem de Parâmetro" cor="#fa4" >
-                <Aluno nome="Zezin da Silva" nota={8} />
+            <Card titulo="Relógio Digital" cor="#fa4" >
+                <RelogioDigital />
             </Card>
 
-            <Card titulo="Exemplo 02" cor="#bb4" >
-                <h3>Aleatório</h3>
+            <Card titulo="Barra de Progresso Interativa" cor="#bb4" >
+                <BarraProgresso valor={50} />
             </Card>
 
-            <Card titulo="Desafio 03 - Lista" >
-                
-                <Turma turma="Front end" />
-
+            <Card titulo="Sistema de Votação" >
+                <Votacao />
             </Card>
-
-            <Card titulo="Desafio 04 - Exercício" >
-                Exercício 2 da lista
-            </Card>
-
-            <Card titulo="Desafio 05 - Componentes Filhos" >
-
-                <Familia sobrenome="Martins" >
-                    <Membro nome="Zezin" sobreNome="Silverio" />
-                    <Membro nome="Mariazinha" sobreNome="XXXXXX" />
-                    <Membro nome="Pedrin" sobreNome="Martins" nivel={1} />
-                    <Membro nome="Gustin" sobreNome="Martins" nivel={2} />
-                </Familia>
-
-                <Familia sobrenome="Perreira" >
-                    <Membro nome="Alberto" />
-                    <Membro nome="Marquin"  nivel={1} />
-                    <Membro nome="Betin"  nivel={3} />
-                </Familia>
-                
-            </Card>
-
-
         </div>
     )    
 }
