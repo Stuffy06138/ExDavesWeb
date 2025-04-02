c0e8529bee2c74a8b5e0f2b624bd2c4f632a9aef
Stuffy06138 <bernardoadribeiro03@gmail.com>
2025-04-02 11:50:29 -0300

    valor aleatorio

---
+++ projetoDaves1/src/components/RandomNumber/randomNumber.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './randomNumber.css'
+
+function randomNumber ({ min, max }) {
+    const [number, setNumber] = useState(
+      Math.floor(Math.random() * (max - min + 1)) + min
+    );
+  
+    return (
+      <div className="container">
+        <h2 className="title">Valor Aleatório</h2>
+        <p className="text"><strong>Valor Mínimo:</strong> {min}</p>
+        <p className="text"><strong>Valor Máximo:</strong> {max}</p>
+        <p className="value">Valor Escolhido: {number}</p>
+      </div>
+    );
+  };
+
+  export default randomNumber;
