787c2d02f3ac96ac8320cc86ee45f58cea9fa051
Stuffy06138 <bernardoadribeiro03@gmail.com>
2025-04-02 12:23:28 -0300

    tabela

--- projetoDaves1/src/components/Table/table.jsx
+++ projetoDaves1/src/components/Table/table.jsx
@@ -1,3 +1,27 @@
 import { useState } from 'react'
 import './table.css'
 
+function table ({ products }) {
+    return (
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Nome</th>
+            <th>Preço</th>
+          </tr>
+        </thead>
+        <tbody>
+          {products.map((product) => (
+            <tr key={product.id}>
+              <td>{product.id}</td>
+              <td>{product.name}</td>
+              <td>{product.price}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    );
+  };
+
+export default table;
